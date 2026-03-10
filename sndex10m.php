c57fe8f14a4e4458d25861611d9312f8e118e812
rzmtv777 <xayitboyevdiyorbek21@gmail.com>
2026-03-10 17:15:52 +0500

    10-mart php oop

---
+++ sndex10m.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP OOP + Superglobals</title>
+</head>
+<body>
+
+<form method="POST" action="process.php">
+    Ism: <input type="text" name="name"><br><br>
+    Yosh: <input type="number" name="age"><br><br>
+    <button type="submit">Yuborish</button>
+</form>
+
+</body>
+</html>
